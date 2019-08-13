099bc3033cf767bfcb8143d2d7be3ba696faf41c
watsuyo <dmwtpdmwtp24@gmail.com>
2019-08-13 12:43:29 +0900

    fix: エラーハンドリング

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,16 +7,12 @@ module.exports = {
   parserOptions: {
     parser: '@typescript-eslint/parser'
   },
-  extends: [
-    '@nuxtjs',
-    '@vue/typescript',
-    'plugin:nuxt/recommended'
-  ],
+  extends: ['@nuxtjs', '@vue/typescript', 'plugin:nuxt/recommended'],
   // add your custom rules here
   rules: {
-    'no-methods-on-designsystem': 'error',
-    'no-directly-use-process': 'error',
-    'no-javascript-vue-component': 'error',
+    // 'no-methods-on-designsystem': 'error',
+    // 'no-directly-use-process': 'error',
+    // 'no-javascript-vue-component': 'error',
     'import/order': 0
   }
 }
--- src/store/userEvents.ts
+++ src/store/userEvents.ts
@@ -14,13 +14,22 @@ export const getters: VuexGetters<State, Getters> = {
 
 export const mutations: VuexMutations<State, Mutations> = {
   setUserEvents(state: State, res: any) {
+    console.log('s')
     state.userEvents = res
   }
 }
 
 export const actions: VuexActions<State, Actions, Getters, Mutations> = {
   async fetchUserEvents(ctx) {
-    const res = await axios.get(`${baseUrl}event?nickname=watuyo_2`)
-    ctx.commit('setUserEvents', res)
+    await axios
+      .get(`${baseUrl}event?nickname=watuyo_2`)
+      .then(res => {
+        console.log(res.status)
+        console.log(res.data)
+        ctx.commit('setUserEvents', res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 }
